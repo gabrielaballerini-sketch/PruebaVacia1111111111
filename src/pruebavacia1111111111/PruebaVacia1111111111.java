b2814fcbd47e10ba145b5da7bc248bea5a0e2178
Gabriela Ballerini <gabrielaballerini@gmail.com>
2025-08-25 12:33:31 -0300

    Initial commit

---
+++ src/pruebavacia1111111111/PruebaVacia1111111111.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebavacia1111111111;
+
+/**
+ *
+ * @author Capotes
+ */
+public class PruebaVacia1111111111 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("Mucha nada");
+        
+        
+        
+    }
+    
+}
