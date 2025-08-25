557e64c5aaf1e3550e5b12630b66b294c24318af
Gabriela Ballerini <gabrielaballerini@gmail.com>
2025-08-25 13:28:06 -0300

    Cambiio 22

---
+++ src/pruebavacia1111111111/Nuevita.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebavacia1111111111;
+
+/**
+ *
+ * @author Capotes
+ */
+public class Nuevita {
+    
+    
+    private String helados;
+
+    public Nuevita(String helados) {
+        this.helados = helados;
+    }
+
+    public String getHelados() {
+        return helados;
+    }
+
+    public void setHelados(String helados) {
+        this.helados = helados;
+    }
+    
+    
+    
+    
+    
+}
